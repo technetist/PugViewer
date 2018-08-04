ca51a9a3eed682ad95611bcf3183989570db0d95
Adrien Maranville <Adrien@Maranville.IO>
2018-08-04 14:26:44 -0600

    Wrapping up base project

--- PugViewer/DetailViewController.swift
+++ PugViewer/DetailViewController.swift
@@ -10,9 +10,14 @@ import Cocoa
 
 class DetailViewController: NSViewController {
 
+    @IBOutlet var imageView: NSImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do view setup here.
     }
     
+    func imageSelected(name: String) {
+        imageView.image = NSImage(named: NSImage.Name(rawValue: name))
+    }
 }
