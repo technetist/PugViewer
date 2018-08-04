632131327c65d6a03178ead58f5a3f47b77e1f25
Adrien Maranville <Adrien@Maranville.IO>
2018-08-04 12:02:15 -0600

    Initial Commit

---
+++ PugViewer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  PugViewer
+//
+//  Created by Adrien Maranville on 8/4/18.
+//  Copyright © 2018 Adrien Maranville. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ PugViewer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  PugViewer
+//
+//  Created by Adrien Maranville on 8/4/18.
+//  Copyright © 2018 Adrien Maranville. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
