6cb8cc06504f2936dc8b82a24629137dbe2afc44
Adrien Maranville <Adrien@Maranville.IO>
2018-08-04 12:47:11 -0600

    Connecting View Controllers

---
+++ PugViewer/DetailViewController.swift
@@ -0,0 +1,18 @@
+//
+//  DetailViewController.swift
+//  PugViewer
+//
+//  Created by Adrien Maranville on 8/4/18.
+//  Copyright © 2018 Adrien Maranville. All rights reserved.
+//
+
+import Cocoa
+
+class DetailViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
---
+++ PugViewer/SourceViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SourceViewController.swift
+//  PugViewer
+//
+//  Created by Adrien Maranville on 8/4/18.
+//  Copyright © 2018 Adrien Maranville. All rights reserved.
+//
+
+import Cocoa
+
+class SourceViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
