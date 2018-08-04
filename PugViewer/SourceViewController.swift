236fcf010b097e1c7aa522cdec8e162c43fc53c7
Adrien Maranville <Adrien@Maranville.IO>
2018-08-04 13:49:10 -0600

    Pulling in images to the table view

--- PugViewer/SourceViewController.swift
+++ PugViewer/SourceViewController.swift
@@ -8,13 +8,34 @@
 
 import Cocoa
 
-class SourceViewController: NSViewController {
+class SourceViewController: NSViewController, NSTableViewDataSource, NSTableViewDelegate {
 
     @IBOutlet var tableView: NSTableView!
+    var pictures = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do view setup here.
+        
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.contains("Pug") {
+                pictures.append(item)
+            }
+        }
+    }
+    
+    func numberOfRows(in tableView: NSTableView) -> Int {
+        return pictures.count
+    }
+    
+    func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
+        guard let vw = tableView.makeView(withIdentifier: tableColumn!.identifier, owner: self) as? NSTableCellView else {return nil}
+        vw.textField?.stringValue = pictures[row]
+        
+        return vw
     }
     
 }
